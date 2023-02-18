057162d4827895ef72905bbfd9c2c28db4ce5eee
Dmitrian23 <dimonio1991@mail.ru>
2023-02-19 01:44:22 +0300

    Задача 060

---
+++ 060/Program.cs
@@ -0,0 +1,35 @@
+﻿// Задача_№60 Показать двумерный массив размером m×n заполненный целыми случайными числами
+
+System.Console.WriteLine("Введите желаемое количество строк:");
+int N=Convert.ToInt32(Console.ReadLine());
+
+System.Console.WriteLine("Введите желаемое колличество столбцов:");
+int M=Convert.ToInt32(Console.ReadLine());
+
+int[,] Random2DArray(int N, int M, int min, int max)
+{
+    int[,] a=new int[N,M];
+Random random=new Random();
+for(int i=0;i<a.GetLength(0);i++)
+for(int j=0;j<a.GetLength(1);j++)
+    a[i,j]=random.Next(min,max+1); 
+    return a;
+}
+void Print2DArray(int[,] a)
+{
+
+for(int i=0;i<a.GetLength(0);i++)
+{
+    System.Console.WriteLine();
+for(int j=0;j<a.GetLength(1);j++)
+ System.Console.Write($"{a[i,j],5}|");
+ System.Console.WriteLine();
+}
+}
+
+int[,] a=Random2DArray(N,M,0,10);
+Print2DArray(a);
+
+
+
+
